112a0fc0b6f839061d190c64c6874eaae3421b36
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-05-06 10:44:47 +0600

    add table

--- invoice-MERN - Dashboard/src/components/DashboardComponent.jsx
+++ invoice-MERN - Dashboard/src/components/DashboardComponent.jsx
@@ -15,7 +15,6 @@ const DashboardComponent = () => {
     useEffect(() => {
         dashboard__Request__API().then((result) => {
             if (result) {
-                console.log(result);
                 setData_flow(result?.data_flow[0])
                 setTable_data(result?.table_data)
                 setBar_chat_monthly_report(result?.bar_chat_monthly_report)
@@ -30,7 +29,7 @@ const DashboardComponent = () => {
 
 
     // console.log(data_flow);
-    console.log(today_total_paid_amount);
+    console.log(table_data);
 
 
     const months_report = [
@@ -278,11 +277,11 @@ const DashboardComponent = () => {
                 </div>
                 <div className='grid grid-cols-12 gap-[30px]'>
                     <div className='col-span-6 bg-white rounded-md  p-[20px] mt-[30px]'>
-                        <h2 className='font-semibold text-gray-700 text-[18px]'>Month Payment & Due Report</h2>
+                        <h2 className='font-semibold text-gray-700 text-[18px]'>Monthly payment & due report</h2>
                         <ReactApexChart options={bar_chat_monthly_report_series_options} series={bar_chat_monthly_report_series} type="bar" height={400} />
                     </div>
                     <div className='col-span-6 bg-white rounded-md  p-[20px] mt-[30px]'>
-                        <h2 className='font-semibold text-gray-700 text-[18px]'>Month Sales</h2>
+                        <h2 className='font-semibold text-gray-700 text-[18px]'>Monthly sales report</h2>
                         <ReactApexChart options={bar_chat_monthly_sales_series_options} series={bar_chat_monthly_sales_series} type="area" height={400} />
                     </div>
                 </div>
@@ -302,165 +301,64 @@ const DashboardComponent = () => {
                                         <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
                                             <tr>
                                                 <th className="p-2 whitespace-nowrap">
-                                                    <div className="font-semibold text-left">Name</div>
+                                                    <div className="font-semibold text-left">Invoice ID</div>
                                                 </th>
                                                 <th className="p-2 whitespace-nowrap">
-                                                    <div className="font-semibold text-left">Email</div>
+                                                    <div className="font-semibold text-left">Customer Name</div>
                                                 </th>
                                                 <th className="p-2 whitespace-nowrap">
-                                                    <div className="font-semibold text-left">Spent</div>
+                                                    <div className="font-semibold text-left">Created Date</div>
                                                 </th>
                                                 <th className="p-2 whitespace-nowrap">
-                                                    <div className="font-semibold text-center">Country</div>
+                                                    <div className="font-semibold text-left">Delivery Date</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap">
+                                                    <div className="font-semibold text-left">Paid</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap">
+                                                    <div className="font-semibold text-left">Due</div>
+                                                </th>
+                                                <th className="p-2 whitespace-nowrap">
+                                                    <div className="font-semibold text-left">Total</div>
                                                 </th>
                                             </tr>
                                         </thead>
                                         <tbody className="text-sm divide-y divide-gray-100">
-                                            <tr>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="flex items-center">
-                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                                                            <img
-                                                                className="rounded-full"
-                                                                src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
-                                                                width={40}
-                                                                height={40}
-                                                                alt="Alex Shatov"
-                                                            />
-                                                        </div>
-                                                        <div className="font-medium text-gray-800">
-                                                            Alex Shatov
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left">alexshatov@gmail.com</div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left font-medium text-green-500">
-                                                        $2,890.66
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-lg text-center">🇺🇸</div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="flex items-center">
-                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                                                            <img
-                                                                className="rounded-full"
-                                                                src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-06.jpg"
-                                                                width={40}
-                                                                height={40}
-                                                                alt="Philip Harbach"
-                                                            />
-                                                        </div>
-                                                        <div className="font-medium text-gray-800">
-                                                            Philip Harbach
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left">philip.h@gmail.com</div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left font-medium text-green-500">
-                                                        $2,767.04
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-lg text-center">🇩🇪</div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="flex items-center">
-                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                                                            <img
-                                                                className="rounded-full"
-                                                                src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-07.jpg"
-                                                                width={40}
-                                                                height={40}
-                                                                alt="Mirko Fisuk"
-                                                            />
-                                                        </div>
-                                                        <div className="font-medium text-gray-800">
-                                                            Mirko Fisuk
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left">mirkofisuk@gmail.com</div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left font-medium text-green-500">
-                                                        $2,996.00
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-lg text-center">🇫🇷</div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="flex items-center">
-                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                                                            <img
-                                                                className="rounded-full"
-                                                                src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-08.jpg"
-                                                                width={40}
-                                                                height={40}
-                                                                alt="Olga Semklo"
-                                                            />
-                                                        </div>
-                                                        <div className="font-medium text-gray-800">
-                                                            Olga Semklo
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left">olga.s@cool.design</div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left font-medium text-green-500">
-                                                        $1,220.66
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-lg text-center">🇮🇹</div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="flex items-center">
-                                                        <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                                                            <img
-                                                                className="rounded-full"
-                                                                src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-09.jpg"
-                                                                width={40}
-                                                                height={40}
-                                                                alt="Burak Long"
-                                                            />
-                                                        </div>
-                                                        <div className="font-medium text-gray-800">
-                                                            Burak Long
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left">longburak@gmail.com</div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-left font-medium text-green-500">
-                                                        $1,890.66
-                                                    </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
-                                                    <div className="text-lg text-center">🇬🇧</div>
-                                                </td>
-                                            </tr>
+                                            {
+                                                table_data.map((item, index) =>
+                                                    <tr key={index}>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left">{item?.invoiceID}</div>
+                                                        </td>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left">{item?.customerName}</div>
+                                                        </td>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left">{item?.startDate?.slice(0, 10)}</div>
+                                                        </td>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left">{item?.deliveryDate?.slice(0, 10)}</div>
+                                                        </td>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left font-medium text-green-500">
+                                                                {item?.payment}
+                                                            </div>
+                                                        </td>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left font-medium text-red-500">
+                                                                {item?.due}
+                                                            </div>
+                                                        </td>
+                                                        <td className="p-2 whitespace-nowrap">
+                                                            <div className="text-left font-bold text-gray-800">
+                                                                {item?.total}
+                                                            </div>
+                                                        </td>
+
+                                                    </tr>)
+                                            }
+
+
                                         </tbody>
                                     </table>
                                 </div>
